55f84a6ed6841c0aa3c814991d29527c4d08c905
Alejandro Aguayo-Ortiz <aaguayoo92@gmail.com>
2023-08-17 23:51:49 -0600

    feat: Se comprobó flujo para dist uniforme cdf, pdf, sample and display

--- python/binaryStatistics/base_distribution.py
+++ python/binaryStatistics/base_distribution.py
@@ -1,37 +1,31 @@
 """Base distribution class."""
-
 from functools import partial
-from typing import Callable
 
 import numpy as np
+from IPython.display import display
 from pydantic.dataclasses import dataclass
 from scipy import interpolate
+from sympy import integrate, lambdify, oo, symbols
 
 
 @dataclass
 class BaseDistribution:
     """Base distribution class."""
 
-    dist_function: Callable
-    dist_parameters: dict = None
-
-    def __post_init_post_parse__(self):
+    def __init__(self, dist_parameters):
         """Post init section.
 
         This built-in method is calle when the BaseDistribution class
         is instantiated. This function creates a self._pdf `partial` function
         which allows you to partially initialize a function.
         """
-        if not self.dist_parameters:
-            self.dist_parameters = {}
-
-        self._pdf = partial(self.dist_function, **self.dist_parameters)
+        self._pdf = partial(self.distribution, **dist_parameters)
 
     def __compute_pdf_norm(self, x: np.array):
         """Compute PDF norm."""
         self.norm_factor = (x.max() - x.min()) / len(x)
         if not hasattr(self, "norm"):
-            self.norm = sum(self._pdf(value) * self.norm_factor for value in x)
+            self.norm = 1.0 / sum(self._pdf(value) * self.norm_factor for value in x)
 
     def cdf(self, x: np.array):
         """Cumulative Distribution Function."""
@@ -40,10 +34,37 @@ class BaseDistribution:
         cumulative_sum = np.cumsum(pdf_values * self.norm_factor)
         return cumulative_sum / cumulative_sum[-1]
 
-    def pdf(self, x, isNormalized: bool = False):
+    def display_cdf(self, value: float = None):
+        """Display CDF."""
+        t, x = symbols("t x")
+        cdf = integrate(self._pdf(t), (t, -oo, x))
+        display(cdf)
+        try:
+            return lambdify(x, cdf)(value)
+        except Exception:
+            return None
+
+    def display_pdf(self, value: float = None):
+        """Display PDF."""
+        x = symbols("x")
+        pdf = self._pdf(x)
+        display(pdf)
+        try:
+            return lambdify(x, pdf)(value)
+        except Exception:
+            return None
+
+    def distribution(self, x, **kwargs):
+        """Distribution function."""
+        raise NotImplementedError(
+            "Distribution function not implemented. "
+            "It could be normalized or in general form."
+        )
+
+    def pdf(self, x):
         """Probability Distribution Function."""
         self.__compute_pdf_norm(x)
-        return self._pdf(x) / self.norm
+        return self._pdf(x) * self.norm
 
     def random_sample(self, x_min: float, x_max: float, size: int):
         """Random sample distribution."""
@@ -52,5 +73,5 @@ class BaseDistribution:
 
         cdf_inv = interpolate.interp1d(cdf, x)
 
-        x_new = np.random.uniform(0.001, 0.999, size=size)
+        x_new = np.random.uniform(0.01, 0.99, size=size)
         return cdf_inv(x_new)
