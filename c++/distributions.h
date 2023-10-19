beb309ced6acc220de745c12681afe2629840ff3
Alejandro Aguayo-Ortiz <aaguayoo92@gmail.com>
2023-10-18 21:21:28 -0600

    feat: Se mejoró repositorio

--- c++/distributions.h
+++
@@ -1,3 +0,0 @@
-double power_law(double x);
-double thermal(double x);
-double uniform(double x);
--- c++/plot.h
+++
@@ -1,6 +0,0 @@
-#define _USE_MATH_DEFINES
-#include <cmath>
-
-using namespace std;
-
-void plot(const vector<double>& hist, long bins, string color, double alpha, bool density, bool cumulative, string parameter, string units, string save_file);
